9d8301028dbf8ed715f830cd8e34f9fa1e80ffa6
renanm <38300927+renanm@users.noreply.github.com>
2023-09-21 19:18:26 -0300

    Update atividade11.cpp

--- condicional/atividade11.cpp
+++ condicional/atividade11.cpp
@@ -4,7 +4,7 @@ main() {
 	float salario;
 	printf("Informe o salario: ");
 	scanf("%f", &salario);
-	if (salario < 1500) {
+	if (salario <= 1500) {
 		printf("O valor do salario reajustado e: R$ %.2f", salario * 1.3);
 	} else {
 		printf("O valor do salario reajustado e: R$ %.2f", salario * 1.15);
