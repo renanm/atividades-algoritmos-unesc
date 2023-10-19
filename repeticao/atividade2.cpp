bd438b5ee65cb728da14a0ca12dc7b50a690603e
renanm <38300927+renanm@users.noreply.github.com>
2023-10-19 13:42:41 -0300

    Update atividade2.cpp

--- repeticao/atividade2.cpp
+++ repeticao/atividade2.cpp
@@ -2,12 +2,12 @@
 
 main() {
 	float total = 0, numero;
+	
 	for (int i = 0; i < 4; i++) {
 		printf("Informe o valor do numero: ");
 		scanf("%f", &numero);
 		total += numero;
 	}
 	
-	printf("Media: %f\n\n", total/4);
-	
+	printf("Media: %f\n\n", total/4);	
 }
